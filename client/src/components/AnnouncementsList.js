6278e7ab0d5c25d8668cfb25f0b196b115587065
wildan phidrai-ngam <wildan8376@gmail.com>
2023-12-24 22:56:22 +0700

    a

--- client/src/components/AnnouncementsList.js
+++ client/src/components/AnnouncementsList.js
@@ -1,24 +1,34 @@
-import { Table } from "react-bootstrap";
+import { useState } from "react";
 import { Card } from "react-bootstrap";
 
 export default function AnnouncementsList(props) {
   console.log(props);
+
+  const [hoveredCard, setHoveredCard] = useState(null);
+
   if (props.data) {
     return (
       <div>
-        <Table hover>
-          <tbody>
-            {props.data.map((d) => (
-              <tr>
-                <Card key={d.id} className="mb-3" borderless>
-                  <Card.Header>{d.course_name}</Card.Header>
-                  <Card.Body>{d.announcement.publish_datetime}</Card.Body>
-                </Card>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
+        {props.data.map((d) => (
+          <Card
+            key={d.id}
+            className={`mb-3 ${hoveredCard === d.id ? "custom-hover" : ""}`}
+            borderless
+            onMouseOver={() => setHoveredCard(d.id)}
+            onMouseOut={() => setHoveredCard(null)}
+            style={{
+              transition: "background-color 0.3s",
+              backgroundColor: hoveredCard === d.id ? "#596FB7" : "inherit",
+              cursor: "pointer",
+            }}
+          >
+            <Card.Header>{d.course_name}</Card.Header>
+            <Card.Body>{d.announcement.publish_datetime}</Card.Body>
+          </Card>
+        ))}
       </div>
     );
   }
+
+  return null; // Handle the case where props.data is not available
 }
