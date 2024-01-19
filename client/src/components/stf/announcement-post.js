27463d174654b32f486564044c5b6f06e167e9a2
wildan phidrai-ngam <wildan8376@gmail.com>
2024-01-20 00:01:03 +0700

    post successfully!!!

--- client/src/components/stf/announcement-post.js
+++ client/src/components/stf/announcement-post.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
-import { Form, Button, Col, Container, Row } from "react-bootstrap";
+import { Form, Button, Col, Container, Row, Modal } from "react-bootstrap";
 import axios from "axios";
+import { useLocation } from "react-router-dom";
+import { useDataContextStf } from "../../utils/stf-context";
 
 axios.defaults.baseURL =
   process.env.REACT_APP_BASE_URL || "http://localhost:1337";
@@ -11,26 +13,60 @@ export default function AnnouncementPost() {
   const [publishDatetime, setPublishDatetime] = useState(null);
   const [maxScore, setMaxScore] = useState(0);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    const response = axios.post("/api/announcements", {
+  const [showModal, setShowModal] = useState(false);
+  const { pathname } = useLocation();
+  const myData = useDataContextStf();
+  const courseId = myData?.courses?.find(
+    (d) => d.name?.split(" ")[0] == pathname.split("/")[3]
+  ).id;
+  const userId = myData?.id;
+
+  const handleSubmit = async (e) => {
+    const response = await axios.post("/api/announcements", {
       data: {
         title: title,
         description: description,
         publish_datetime: publishDatetime,
         full_score: maxScore,
+        course: parseInt(courseId),
+        announcer: userId,
       },
     });
-    console.log(title, description, publishDatetime, maxScore);
+    window.location.reload();
   };
 
   return (
     <Container>
-      <Form>
+      <Modal
+        show={showModal}
+        // onHide={() => setShowModal(false)}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>ท่านแน่ใจว่าต้องการโพสต์ประกาศใช่หรือไม่</Modal.Title>
+        </Modal.Header>
+        {/* <Modal.Body>ท่านแน่ใจว่าต้องการโพสต์ประกาศใช่หรือไม่?</Modal.Body> */}
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            ยกเลิก
+          </Button>
+          <Button variant="primary" type="button" onClick={handleSubmit}>
+            ยืนยัน
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <Form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setShowModal(true);
+        }}
+      >
         <Row className="mb-3">
           <Col sm={{ span: 6, offset: 3 }}>
             <Form.Label className="text-center">หัวข้อ</Form.Label>
             <Form.Control
+              required
               type="text"
               placeholder="หัวข้อประกาศ"
               onChange={(e) => setTitle(e.target.value)}
@@ -41,6 +77,7 @@ export default function AnnouncementPost() {
           <Col sm={{ span: 6, offset: 3 }}>
             <Form.Label className="text-center">รายละเอียด</Form.Label>
             <Form.Control
+              required
               type="text"
               placeholder="เพิ่มรายละเอียด"
               onChange={(e) => setDescription(e.target.value)}
@@ -51,6 +88,7 @@ export default function AnnouncementPost() {
           <Col sm="6">
             <Form.Label>คะแนนเต็ม</Form.Label>
             <Form.Control
+              required
               type="number"
               placeholder="คะแนนเต็ม"
               onChange={(e) => setMaxScore(e.target.value)}
@@ -59,6 +97,7 @@ export default function AnnouncementPost() {
           <Col sm="6">
             <Form.Label>เวลาประกาศ</Form.Label>
             <Form.Control
+              required
               type="date"
               placeholder="เพิ่มรายละเอียด"
               onChange={(e) => setPublishDatetime(e.target.value)}
@@ -67,8 +106,8 @@ export default function AnnouncementPost() {
         </Row>
         <Row className="text-center">
           <Col>
-            <Button variant="primary" type="submit" onClick={handleSubmit}>
-              Submit
+            <Button variant="primary" type="submit">
+              ยืนยัน
             </Button>
           </Col>
         </Row>
--- client/src/pages/stf/AnnouncementsPageStf.js
+++ client/src/pages/stf/AnnouncementsPageStf.js
@@ -1,7 +1,7 @@
 import StaffNavbar from "../../components/stf/staff-navbar";
 import AnnouncementsList from "../../components/stf/announcements-list";
 import { DataProviderStf } from "../../utils/stf-context";
-import AnnouncementPost from "../../components/stf/announcement-page";
+import AnnouncementPost from "../../components/stf/announcement-post";
 
 function AnnouncementsPageStf() {
   return (
